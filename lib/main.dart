dee997b9f2dc0594c00610bcabf9529b87d3e432
Nomonjon <ummugulusum2425@gmail.com>
2024-07-08 19:56:27 +0500

    test multi branch

--- lib/main.dart
+++ lib/main.dart
@@ -8,20 +8,18 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
-    SystemChrome.setSystemUIOverlayStyle(
-        const SystemUiOverlayStyle(statusBarColor: Color(0xff252429), statusBarIconBrightness:Brightness.light));
+    SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(statusBarColor: Color(0xff252429), statusBarIconBrightness: Brightness.light));
     return MaterialApp(
-      debugShowCheckedModeBanner : true,
-
+      debugShowCheckedModeBanner: true,
       title: 'Flutter Demo',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
       home: SplashScreen(),
-
     );
   }
 }

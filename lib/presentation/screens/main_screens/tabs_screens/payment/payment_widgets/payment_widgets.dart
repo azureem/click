3bf68a789641bd44db427ac8c6bf6c240836ea01
Nomonjon <ummugulusum2425@gmail.com>
2024-07-11 17:09:13 +0500

    PaymentScreen

--- lib/presentation/screens/main_screens/tabs_screens/payment/payment_widgets/payment_widgets.dart
+++ lib/presentation/screens/main_screens/tabs_screens/payment/payment_widgets/payment_widgets.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-
 import '../../../../../../utils/components/colors/colors.dart';
 
 class ItemCategory extends StatelessWidget {
@@ -10,16 +9,16 @@ class ItemCategory extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 50,
+      height: 60,
       width: MediaQuery.of(context).size.width,
-      margin: EdgeInsets.symmetric(horizontal: 15, vertical: 10),
-      padding: EdgeInsets.symmetric(vertical: 10, horizontal: 10),
+      margin: const EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+      padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 10),
       decoration: BoxDecoration(borderRadius: BorderRadius.circular(10), color: ClickColors.containerWidgetColor),
       child: Row(
         children: [
           Image.network(image),
-          SizedBox(width: 10,),
-          Text(title, style: TextStyle(color: Colors.white, ),)
+          const SizedBox(width: 10),
+          Text(title, style: const TextStyle(color: Colors.white))
         ],
       ),
     );

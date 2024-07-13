e09e31355bb984d7a51be937751e8687a39da820
Nomonjon <ummugulusum2425@gmail.com>
2024-07-13 18:17:29 +0500

    ReportScreen

--- lib/presentation/screens/main_screens/tabs_screens/reports/report_screen.dart
+++ lib/presentation/screens/main_screens/tabs_screens/reports/report_screen.dart
@@ -1,3 +1,4 @@
+import 'package:click_uz/presentation/screens/main_screens/tabs_screens/reports/report_widget/report_widget.dart';
 import 'package:flutter/material.dart';
 
 import '../../../../../utils/components/colors/colors.dart';
@@ -10,12 +11,13 @@ class ReportScreen extends StatefulWidget {
 }
 
 class _ReportScreenState extends State<ReportScreen> {
-
-  bool isChecked=false;
+  bool isChecked = false;
+  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      key: _scaffoldKey,
       appBar: AppBar(
         forceMaterialTransparency: true,
         title: const Center(
@@ -48,49 +50,199 @@ class _ReportScreenState extends State<ReportScreen> {
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 Expanded(
+                  flex: 1,
                   child: Container(
+                    decoration: BoxDecoration(
+                        borderRadius: const BorderRadius.only(topLeft: Radius.circular(10), bottomLeft: Radius.circular(10)),
+                        color: (!isChecked) ? const Color(0xFF0274F1) : ClickColors.bodyBackground),
                     child: InkWell(
-                      onTap: (){
+                      onTap: () {
                         setState(() {
-                          isChecked=!isChecked;
+                          isChecked = !isChecked;
                         });
                       },
-                      child: Center(
+                      child: const Center(
                           child: Text(
                         "Click bo'yicha hisobot",
                         style: TextStyle(color: Colors.white),
                       )),
                     ),
-                    decoration: BoxDecoration(
-                        borderRadius: BorderRadius.only(topLeft: Radius.circular(10), bottomLeft: Radius.circular(10)), color: (!isChecked)?Color(0xFF0274F1):ClickColors.bodyBackground),
                   ),
-                  flex: 1,
                 ),
                 Expanded(
+                  flex: 1,
                   child: Container(
+                    decoration: BoxDecoration(
+                        borderRadius: const BorderRadius.only(topRight: Radius.circular(10), bottomRight: Radius.circular(10)),
+                        color: (isChecked) ? const Color(0xFF0274F1) : ClickColors.bodyBackground),
                     child: InkWell(
-                      onTap: (){
+                      onTap: () {
                         setState(() {
-                          isChecked=!isChecked;
+                          isChecked = !isChecked;
                         });
                       },
-                      child: Center(
+                      child: const Center(
                           child: Text(
                         "Monitoring",
                         style: TextStyle(color: Colors.white),
                       )),
                     ),
-                    decoration: BoxDecoration(
-                        borderRadius: BorderRadius.only(topRight: Radius.circular(10), bottomRight: Radius.circular(10)), color:(isChecked)? Color(0xFF0274F1):ClickColors.bodyBackground),
                   ),
-                  flex: 1,
                 ),
               ],
+            ),
+          ),
+          Expanded(
+            child: ListView.builder(
+              itemBuilder: (context, index) {
+                return itemReport(context);
+              },
+              itemCount: 15,
+              physics: const BouncingScrollPhysics(),
             ),
           )
         ],
       ),
       backgroundColor: ClickColors.bodyBackground,
+      floatingActionButton: InkWell(
+        onTap: () {
+          _scaffoldKey.currentState?.openEndDrawer();
+        },
+        child: Container(
+          height: 60,
+          width: 60,
+          decoration: BoxDecoration(borderRadius: BorderRadius.circular(30), color: const Color(0xFF0274F1)),
+          child: const Icon(
+            Icons.menu,
+            color: Colors.white,
+            size: 35,
+          ),
+        ),
+      ),
+      endDrawer: Drawer(
+        backgroundColor: ClickColors.bodyBackground,
+        shape: const RoundedRectangleBorder(),
+        child: Column(
+          children: [
+            const SizedBox(
+              height: 40,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                InkWell(
+                  onTap:(){
+                    _scaffoldKey.currentState?.closeEndDrawer();
+                  },
+                  child: const Padding(
+                    padding: EdgeInsets.only(left: 15),
+                    child: Icon(
+                      Icons.arrow_back_ios,
+                      color: Colors.white,
+                      size: 20,
+                    ),
+                  ),
+                ),
+                const Text(
+                  "Filtr",
+                  style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold, fontSize: 18),
+                ),
+                Container(
+                  width: 30,
+                )
+              ],
+            ),
+            Container(
+              height: 50,
+              margin: const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10), color: ClickColors.containerWidgetColor, border: Border.all(color: Colors.white24)),
+              child: const Row(
+                children: [
+                  Expanded(
+                    child: Padding(
+                      padding: EdgeInsets.only(left: 15),
+                      child: TextField(
+                        decoration: InputDecoration(border: InputBorder.none, hintText: 'Qidiruv', hintStyle: TextStyle(color: Colors.white24)),
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(right: 10),
+                    child: Icon(
+                      Icons.search,
+                      color: Colors.white24,
+                    ),
+                  )
+                ],
+              ),
+            ),
+            const Padding(
+              padding: EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+              child: Row(
+                children: [
+                  Text(
+                    "Karta bo'yicha",
+                    style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold, fontSize: 18),
+                  ),
+                  Spacer(),
+                  Icon(
+                    Icons.add,
+                    color: Color(0xFF0274F1),
+                    size: 35,
+                  )
+                ],
+              ),
+            ),
+            const Padding(
+              padding: EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+              child: Row(
+                children: [
+                  Text(
+                    "Sana bo'yicha",
+                    style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold, fontSize: 18),
+                  ),
+                  Spacer(),
+                  Icon(
+                    Icons.add,
+                    color: Color(0xFF0274F1),
+                    size: 35,
+                  )
+                ],
+              ),
+            ),
+            Spacer(),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+              child: Row(
+                children: [
+                  InkWell(
+                    onTap:(){
+                      _scaffoldKey.currentState?.closeEndDrawer();
+                    },
+                    child: const Text(
+                      "Yopish",
+                      style: TextStyle(color: Colors.white12, fontWeight: FontWeight.bold, fontSize: 14),
+                    ),
+                  ),
+                  const Spacer(),
+                  Container(
+                    height: 50,
+                    width: 130,
+                    decoration: BoxDecoration(borderRadius: BorderRadius.circular(10), color: const Color(0xFF0274F1)),
+                    child: const Center(
+                      child: Text(
+                        "Qo'llash",
+                        style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold, fontSize: 14),
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
--- lib/presentation/screens/main_screens/tabs_screens/reports/report_widget/report_widget.dart
+++ lib/presentation/screens/main_screens/tabs_screens/reports/report_widget/report_widget.dart
@@ -1,17 +1,45 @@
 import 'package:click_uz/utils/components/colors/colors.dart';
 import 'package:flutter/material.dart';
 
-Widget itemReport(BuildContext context){
+Widget itemReport(BuildContext context) {
   return Container(
     height: 80,
     width: MediaQuery.of(context).size.width,
-    decoration: BoxDecoration(
-      borderRadius: BorderRadius.circular(10),
-      color: ClickColors.containerWidgetColor
-    ),
+    margin: EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+    padding: EdgeInsets.symmetric(horizontal: 10, vertical: 15),
+    decoration: BoxDecoration(borderRadius: BorderRadius.circular(10), color: ClickColors.containerWidgetColor),
     child: Row(
       children: [
-
+        Image.asset(
+          'assets/images/result_1.png',
+          height: 50,
+          width: 50,
+        ),
+        SizedBox(
+          width: MediaQuery.of(context).size.width / 3 + 50,
+          child: Padding(
+            padding: const EdgeInsets.only(left: 10),
+            child: Align(
+                alignment: Alignment.topLeft,
+                child: Text(
+                  "ATTO TRANSPORT TOLOV",
+                  style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold, fontSize: 12),
+                  maxLines: 3,
+                  overflow: TextOverflow.ellipsis,
+                )),
+          ),
+        ),
+        Spacer(),
+        Column(
+          children: [
+            Text("-1700 so'm", style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),),
+            Text("11 iyl 10:11", style: TextStyle(color: Colors.white, fontSize: 11),)
+          ],
+        ),
+        Padding(
+          padding: const EdgeInsets.only(left: 5),
+          child: Icon(Icons.check_circle_outlined, color: Color(0xFF0274F1),size: 30,),
+        )
       ],
     ),
   );
